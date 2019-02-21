2c657ccd270e706c9a48e14baa540dc1bd669768
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-02-21 09:29:59 -0300

    Aula 246-249 -Imagens de Transição -Importação da biblioteca de animação -Criação do model Imagem -Trigger banner -Array de imagens para utilizar no banner

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style } from '@angular/animations';
+
+import { Imagem } from './imagem.model';
 
 @Component({
   selector: 'app-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.css']
+  styleUrls: ['./banner.component.css'],
+  animations: [
+    trigger('banner', [
+      state('escondido', style({
+        opacity: 0
+      })),
+      state('visivel', style({
+        opacity: 1
+      }))
+    ])
+  ]
 })
 export class BannerComponent implements OnInit {
+
+  public estado: string = 'visivel';
+
+  public imagens: Imagem[] = [
+    {estado: 'escondido', url: './assets/banner-acesso/img_1.png'},
+    {estado: 'escondido', url: './assets/banner-acesso/img_2.png'},
+    {estado: 'escondido', url: './assets/banner-acesso/img_3.png'},
+    {estado: 'escondido', url: './assets/banner-acesso/img_4.png'},
+    {estado: 'escondido', url: './assets/banner-acesso/img_5.png'}
+  ];
 
   constructor() { }
 
