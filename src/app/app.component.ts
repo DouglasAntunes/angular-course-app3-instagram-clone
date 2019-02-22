fdc557fc038a0eb2eacd5693bce3c75f6175b91f
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-02-22 19:57:16 -0300

    Aula 263 e 265 -Adicionado o Firebase como Dep -Conexão com o projeto no Firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app3';
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCIZGF5afXGsl44qbVUenQqYOYjh8psgu8',
+      authDomain: 'jta-instagram-clone-2f9e3.firebaseapp.com',
+      databaseURL: 'https://jta-instagram-clone-2f9e3.firebaseio.com',
+      projectId: 'jta-instagram-clone-2f9e3',
+      storageBucket: 'jta-instagram-clone-2f9e3.appspot.com',
+      messagingSenderId: '413258453663'
+    });
+  }
 }
