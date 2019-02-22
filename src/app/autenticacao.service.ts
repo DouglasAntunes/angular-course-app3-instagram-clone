49dbc90428837562405e42e89b1786a899b2d51a
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-02-22 20:04:03 -0300

    Aula 266 -Cadastro no firebase de email e senha

--- src/app/autenticacao.service.ts
+++ src/app/autenticacao.service.ts
@@ -1,10 +1,19 @@
+import * as firebase from 'firebase';
+
 import { Usuario } from './acesso/usuario.model';
 
 export class AutenticacaoService {
 
 
     public cadastrarUsuario(usuario: Usuario): void {
-        console.log('chegamos até o serviço: ', usuario);
+        // console.log('chegamos até o serviço: ', usuario);
+        firebase.auth().createUserWithEmailAndPassword(
+            usuario.email, usuario.senha
+        ).then((resposta: any) => {
+            console.log(resposta);
+        }).catch((erro: Error) => {
+            console.log('Erro na criação de usuário: ', erro);
+        })
     }
 
 
