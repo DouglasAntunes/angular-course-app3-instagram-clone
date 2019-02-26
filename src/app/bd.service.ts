e9f3f6d11d356eedd3b857b718a1e3aaf266a91c
DouglasAntunes <DouglasAntunes@users.noreply.github.com>
2019-02-26 19:53:25 -0300

    Aula 293 -Ajuste da Ordem de gravação do titulo e imagem no Firebase.

--- src/app/bd.service.ts
+++ src/app/bd.service.ts
@@ -14,34 +14,34 @@ export class BdService {
     ) { }
 
     public publicar(publicacao: any): void {
-        console.log(publicacao);
+        // console.log(publicacao);
 
-        const nomeImagem = Date.now();
-
-        firebase.storage().ref()
-            .child(`imagens/${nomeImagem}`)
-            .put(publicacao.imagem)
-            .on(firebase.storage.TaskEvent.STATE_CHANGED,
-                // acompanhamento do progresso de upload
-                (snapshot: any) => {
-                    // console.log(snapshot);
-                    this.progressoService.status = 'andamento';
-                    this.progressoService.estado = snapshot;
-                },
-                (error: Error) => {
-                    // console.log(error);
-                    this.progressoService.status = 'erro';
-                },
-                // finalização do processo
-                () => {
-                    // console.log('upload completo');
-                    this.progressoService.status = 'concluido';
-                }
+        // Inclusão do título e referencia da imagem
+        firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
+        .push({ titulo: publicacao.titulo })
+        .then((resposta: any) => {
+            const nomeImagem = resposta.key;
+            // Upload da Imagem com o nome da key do documento
+            firebase.storage().ref()
+                .child(`imagens/${nomeImagem}`)
+                .put(publicacao.imagem)
+                .on(firebase.storage.TaskEvent.STATE_CHANGED,
+                    // acompanhamento do progresso de upload
+                    (snapshot: any) => {
+                        // console.log(snapshot);
+                        this.progressoService.status = 'andamento';
+                        this.progressoService.estado = snapshot;
+                    },
+                    (error: Error) => {
+                        // console.log(error);
+                        this.progressoService.status = 'erro';
+                    },
+                    // finalização do processo
+                    () => {
+                        // console.log('upload completo');
+                        this.progressoService.status = 'concluido';
+                    }
             );
-
-        // firebase.database().ref(`publicacoes/${btoa(publicacao.email)}`)
-        //     .push({
-        //         titulo: publicacao.titulo
-        // });
+        });
     }
 }
